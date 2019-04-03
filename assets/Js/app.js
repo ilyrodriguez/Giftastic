a206e5f713953dbe446a7350ccde1c1ad81b8e88
unknown <ilyrodriguez@gmail.com>
2019-04-02 20:15:56 -0400

    need to fix columns and rating locations

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,62 +1,68 @@
 $(document).ready(function() { 
    
-    var gifs = ["Animals", "Food and Drinks", "Cartoons", "Movies", "Music", "nature", "Science"];
+    var topics = ["Animals", "Food and Drinks", "Cartoons", "Movies", "Music", "nature", "Science"];
     
     function displayGifRating() {
 
         var gif = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&limit=10&api_key=dc6zaTOxFJmzC";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=UxNR1uNih1F65bA3EEK3M4XZnDrOhr2A&limit=10";
          
         $.ajax({
           url: queryURL,
           method: "GET"
-        }).then(function(response) {
-            console.log(this);
-            var results = response.rating;
+        })
+        .then(function(response) {
+            console.log(queryURL);
+            console.log(response.data);
 
-            var gifDiv = $("<div class='divGif'>");
-            var showGif = $('<img>');
-            showGif.attr('src', results[i].images.fixed_height_still.url);
-            showGif.attr('title', "Rating: " + results[i].rating);
-			showGif.attr('data-still', results[i].images.fixed_height_still.url);
-			showGif.attr('data-state', 'still');
-			showGif.addClass('gif');
-			showGif.attr('data-animate', results[i].images.fixed_height.url);
-            // var image = $("<img>").attr("src", results[i].images.fixed_height.url);
-            gifDiv.append(p);
-            gifDiv.append(showGif);
-    
-            $("#gif-view").prepend(gifDiv);
+            var results = response.data;
+            for (var i = 0; i < results.length; i++){
+                var gifDiv = $('<div class= divGif>');
+                var showGif = $('<img>');
+                showGif.addClass("gifClick");
+                showGif.attr('src', results[i].images.fixed_height_still.url);
+                showGif.attr('data-still', results[i].images.fixed_height_still.url);
+                showGif.attr('data-animate', results[i].images.fixed_height.url);
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                showGif.attr("data-state", "data-still");
+                gifDiv.append(p);
+                gifDiv.append(showGif);
+                $("#gif-view").prepend(gifDiv);
+            }
         });
     }
-    
+
     function makeButtons(){ 
-        // deletes the gif prior to adding new gif so there are no repeat buttons
         $("#buttons-view").empty();
         // loops through the gif array
-        for (var i = 0; i < gifs.length; i++){
-            // dynamically makes buttons for every gif in the array
+        for (i = 0; i < topics.length; i++){
             var a = $("<button>") 
-            a.addClass("gif-btn"); // add a class
-            a.attr("data-name", gifs[i]); // add a data-attribute
-            a.text(gifs[i]); // make button text
-            $("#buttons-view").append(a); // append the button to buttonsView div
+            a.addClass("gif-btn"); 
+            a.attr("data-name", topics[i]); 
+            a.text(topics[i]); 
+            $("#buttons-view").append(a); 
         }
     }
-    $("#add-gif").on("click", function(event) {
+    // $(".gifClick").on("click", function(){
+        $(document).on("click", ".gifClick", function(){
+        console.log(this);
+        var state = $(this).attr("data-state");
+            if (state === "still"){
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else{
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            };
+        });
+
+    $("#add-gif").on("click", function() {
         event.preventDefault();
         var gif = $("#gif-input").val().trim();
-        gifs.push(gif);
+        topics.push(gif);
         makeButtons();
       });
 
     $(document).on("click", ".gif-btn", displayGifRating);
     makeButtons();
-
-
-
-
-
-
-
 })
